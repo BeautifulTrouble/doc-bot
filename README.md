78d7c4aafd65af850be3d45a2681003034752cbe
Phillip Smith <ps@phillipadsmith.com>
2015-01-27 13:10:07 -0800

    Various minor improvements

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## TODO
 
-* [ ] Ignore subsequent commits of same file (alternate versions): quick thought is to check the commit message for signs of the doc bot
-* [ ] Deal with the fact that pandoc can't convert odt to markdown :thumbsdown: (might need another library, https://github.com/search?utf8=%E2%9C%93&q=odt+html)
-* [ ] Bootstrap the repo on first run
-* [ ] Ignore folders without the right destinations (markdown, msword, openoffice)
+* [x] Ignore subsequent commits of same file (alternate versions): quick thought is to check the commit message for signs of the doc bot
+* [x] Deal with the fact that pandoc can't convert odt to markdown :thumbsdown: (might need another library, https://github.com/search?utf8=%E2%9C%93&q=odt+html). For now, skipping if the added/modified file is .odt
+* [x] Bootstrap the repo on first run
+* [x] ~~Ignore~~ Create folders without the right destinations (markdown, msword, openoffice)
 * [ ] Abilty to set a different Github user to commit as
--- app.pl
+++ app.pl
@@ -2,12 +2,18 @@
 use Mojolicious::Lite;
 use Mojo::JSON qw(decode_json encode_json);
 use Mojo::Util qw(dumper);
+use FindBin qw($Bin);
 use File::Spec::Functions;
 
 my $config = plugin 'JSONConfig';
 app->secrets( [ $config->{'app_secret'} ] );
 plugin Minion => { File => $config->{'minion_db'} };
-my $repo = $config->{'repo'};
+my $repo     = $config->{'repo'};
+my $repo_url = $config->{'repo_url'};
+
+unless ( -d $repo ) {
+    my $git_clone = `git clone $repo_url`;
+}
 
 # Job queue task
 app->minion->add_task(
@@ -46,6 +52,15 @@ app->minion->add_task(
         #### TODO replace this with something more sensible:
         my ( $ext )       = $file =~ /\.(\w+)$/;
         my ( $file_name ) = $file =~ /(.*)\./;
+        if ( $ext eq 'odt' ) {
+            $job->app->log->info( "No support for odt yet..." );
+            return;
+        }
+        unless ( $directories =~ /$format_dirs{ $ext }/ ) {
+            $job->app->log->info(
+                "The file doesn't appear to be in an expected directory..." );
+            return;
+        }
         ####
         my $alts = $format_alts{$ext};
 
@@ -53,8 +68,15 @@ app->minion->add_task(
         for my $to_create ( @$alts ) {
             $job->app->log->info( "Going to create: $to_create" );
 
+            # Do I have an adequate directory to put this file in?
+            my $target_dir = File::Spec->catdir( $repo, $dirs[0], $format_dirs{$to_create} );
+            $job->app->log->info( $target_dir );
+            unless ( -d $target_dir ) {
+                $job->app->log->info( "No target directory, creating..." );
+                mkdir $target_dir;
+            }
             my $cmd
-                = "pandoc -f $formats{ $ext } -t $to_create $repo/$directories$file -o $repo/$dirs[0]/$format_dirs{ $to_create }/$file_name.$to_create";
+                = "pandoc -f $formats{ $ext } -t $to_create $repo/$directories$file -o $target_dir/$file_name.$to_create";
             my $output = `$cmd`;
         }
 
@@ -76,6 +98,15 @@ post '/github' => sub {
     my $commits = $payload->{'commits'};
     my $jobs    = 0;
     for my $commit ( @$commits ) {
+
+        # If this is a doc bot commit, skip it
+        if ( $commit->{'message'} =~ /Doc bot:/g ) {
+            $c->app->log->debug(
+                "Skipping because this is a doc bot commit" );
+            next;
+        }
+
+        # If files are added or changed, add them to the queue
         if ( $commit->{'modified'} || $commit->{'added'} ) {
             my @changes = @{ $commit->{'modified'} };
             push( @changes, @{ $commit->{'added'} } );
@@ -96,7 +127,7 @@ post '/github' => sub {
     if ( $jobs == 0 ) {
         return $c->render(
             json   => { message => "No new or changed files" },
-            status => 400
+            status => 422
         );
     }
 };
